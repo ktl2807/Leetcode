b868f1978cb756f96cd9a1b1f537c5cea7a82f86
KUEI-TSEN LIAO <86973641+ktl2807@users.noreply.github.com>
2023-08-25 14:27:16 -0700

    Time: 39 ms (81.93%), Space: 13.4 MB (92.01%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        dic = {}
+        for i in magazine:
+            if i not in dic:
+                dic[i] =1
+            else:
+                dic[i]+=1
+        
+        for i in ransomNote:
+            if i in dic and dic[i]>0:
+                dic[i]-=1
+            else:
+                return False
+        return True
